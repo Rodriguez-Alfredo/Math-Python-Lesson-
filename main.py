0d85bbeae9a79abcc31ec85d274f276a7991169c
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-09 23:50:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+print(50 + 50) #add
+print(50 -50) #subtract
+print(50 * 50) #multiply
+print(50/50) #divide
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print(50 ** 2) #exponents
+print(50 % 6) #modulo - remainder
+print(50 / 6) #division with remainder (or a float)
+print(50 // 6) #no remainder (only integer)
+
+#Thank you TCM Security
